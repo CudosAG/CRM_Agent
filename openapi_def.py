edd455e819979b4bdbf09bb93b7db3444f81f754
Severin Thöni <ths@m-f.ch>
2024-11-15 11:59:56 +0100

    more escaping for anführungszeichen

--- openapi_def.py
+++ openapi_def.py
@@ -21,7 +21,7 @@ OPENAPI_DEF = """
           {
             "name": "query",
             "in": "query",
-            "description": "Query the crm database. There are three tables in the database: 'organizations', 'people' and 'leads'. The organizations table has the following fields: Organisationsname,Webseite,\"Umsatz (MIO CHF)\",\"Distanz ZH (km)\",zuständig,\"Distanz Chur (km)\",\"primäre E-Mail\",Branche,Rechnungsadresse,\"Rechnung PLZ\",\"Rechnung Postfach\",\"Rechnung Ort\",\"Rechnung Land\",\"Klassifizierung Softwareentwicklung und -testing \",\"Klassifizierung Prüfsysteme\",\"Klassifizierung AI (Künstliche Intelligenz)\",\"Klassifizierung Azure\",\"Klassifizierung Traineeprogramm\",\"Klassifizierung swissICT Booster 50+\",Typ,Beschreibung,Unternehmenszweck,Verwaltungsrat,Zeichnungsberechtigte,Kundenart",
+            "description": "Query the crm database. There are three tables in the database: 'organizations', 'people' and 'leads'. The organizations table has the following fields: Organisationsname,Webseite,\\"Umsatz (MIO CHF)\\",\\"Distanz ZH (km)\\",zuständig,\\"Distanz Chur (km)\\",\\"primäre E-Mail\\",Branche,Rechnungsadresse,\\"Rechnung PLZ\\",\\"Rechnung Postfach\\",\\"Rechnung Ort\\",\\"Rechnung Land\\",\\"Klassifizierung Softwareentwicklung und -testing \\",\\"Klassifizierung Prüfsysteme\\",\\"Klassifizierung AI (Künstliche Intelligenz)\\",\\"Klassifizierung Azure\\",\\"Klassifizierung Traineeprogramm\\",\\"Klassifizierung swissICT Booster 50+\\",Typ,Beschreibung,Unternehmenszweck,Verwaltungsrat,Zeichnungsberechtigte,Kundenart",
             "required": true,
             "schema": {
               "type": "string"
